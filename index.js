1b41eb5ea4f772899ef90bfc1f27722f9ecb7d91
Hunter <38193859+Blockdude@users.noreply.github.com>
2019-03-07 11:27:29 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const ejs = require('ejs');
 const port = process.env.PORT || 5000;
+const path = require('path');
 const app = express();
 
-app.get('/', function(req, res) {
+app.set('view engine', 'ejs')
+.get('/', (req, res) => res.render('/index.ejs'));
+
+/*app.get('/', function(req, res) {
     console.log(`req = ${req}`);
     if(req == ""){
         res.send('<h1>index.here</h1>');
@@ -10,7 +15,7 @@ app.get('/', function(req, res) {
 
     res.send('<h1>Heroku world!</h1>');
     }
-});
+});//*/
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
