8a63199edb70c7a8b963581ca0387da0f6b75507
Hunter <38193859+Blockdude@users.noreply.github.com>
2019-03-07 10:41:02 -0800

    Add files via upload

--- web.js
+++ web.js
@@ -1,18 +1,12 @@
-var express = require('express');
-const https = require('https');
-const os = require('os');
-var app = express();
+const express = require('express');
+const port = process.env.PORT || 5000;
+const app = express();
 
 app.get('/', function(req, res) {
 
 	res.send('<h1>Heroku world!</h1>');
-    //fetch('localhost:3000/weather.html').then(data => res.send(data))
-    //.catch(error => {res.send('');})
-    //res.send('<h1>Hello World!</h1>');
 });
 
-app.listen(3000, function() {
-    console.log('Example app listening on port 3000!');
-
-console.log(os.hostname());
+app.listen(port, () => {
+    console.log('Example app listening on port ${port}!');
 });
